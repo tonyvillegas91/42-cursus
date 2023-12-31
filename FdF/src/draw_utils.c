04d65cd1b96e96ff4f33d4bdd40d26e3f2f447ff
tonyvillegas91 <tony.villegas91@gmail.com>
2023-12-31 15:00:08 +0100

    update 1.5

--- FdF/src/draw_utils.c
+++ FdF/src/draw_utils.c
@@ -15,71 +15,65 @@
 #include <mlx.h>
 #include <math.h>
 
-// Función para aplicar proyección isométrica a las coordenadas (x, y, z).
-void isometric(t_vars *vars, int *x, int *y, int z)
+void	isometric(t_vars *vars, int *x, int *y, int z)
 {
-    int prev_x;
-    int prev_y;
+	int	prev_x;
+	int	prev_y;
 
-    if (!vars->iso)
-        return;
-    prev_x = *x;
-    prev_y = *y;
-    *x = (prev_x - prev_y) * cos(0.523599);
-    *y = -z + (prev_x + prev_y) * sin(0.523599);
+	if (!vars->iso)
+		return ;
+	prev_x = *x;
+	prev_y = *y;
+	*x = (prev_x - prev_y) * cos(0.523599);
+	*y = -z + (prev_x + prev_y) * sin(0.523599);
 }
 
-// Funciones para realizar rotación alrededor del eje X, Y y Z.
-void rot_x(t_vars *vars, int *y, int *z)
+void	rot_x(t_vars *vars, int *y, int *z)
 {
-    float prev_y;
+	float	prev_y;
 
-    prev_y = *y;
-    *y = (prev_y * cos(vars->rot_x)) + (*z * sin(vars->rot_x));
-    *z = (-prev_y * sin(vars->rot_x)) + (*z * cos(vars->rot_x));
+	prev_y = *y;
+	*y = (prev_y * cos(vars->rot_x)) + (*z * sin(vars->rot_x));
+	*z = (-prev_y * sin(vars->rot_x)) + (*z * cos(vars->rot_x));
 }
 
-void rot_y(t_vars *vars, int *x, int *z)
+void	rot_y(t_vars *vars, int *x, int *z)
 {
-    float prev_x;
+	float	prev_x;
 
-    prev_x = *x;
-    *x = (*x * cos(vars->rot_y)) + (*z * sin(vars->rot_y));
-    *z = (-prev_x * sin(vars->rot_y)) + (*z * cos(vars->rot_y));
+	prev_x = *x;
+	*x = (*x * cos(vars->rot_y)) + (*z * sin(vars->rot_y));
+	*z = (-prev_x * sin(vars->rot_y)) + (*z * cos(vars->rot_y));
 }
 
-void rot_z(t_vars *vars, int *x, int *y)
+void	rot_z(t_vars *vars, int *x, int *y)
 {
-    float prev_x;
+	float	prev_x;
 
-    prev_x = *x;
-    *x = (*x * cos(vars->rot_z)) - (*y * sin(vars->rot_z));
-    *y = (prev_x * sin(vars->rot_z)) + (*y * cos(vars->rot_z));
+	prev_x = *x;
+	*x = (*x * cos(vars->rot_z)) - (*y * sin(vars->rot_z));
+	*y = (prev_x * sin(vars->rot_z)) + (*y * cos(vars->rot_z));
 }
 
-// Función para poner un píxel en la imagen con el color dado.
-void img_pixel_put(t_vars *vars, int x, int y, int color)
+void	img_pixel_put(t_vars *vars, int x, int y, int color)
 {
-    int pixel;
+	int	pixel;
 
-    if (y >= WIN_H || x >= WIN_W || y < 0 || x < 0)
-        return;
-
-    pixel = (y * vars->img->sz_l) + (x * (vars->img->bpp / 8));
-
-    // Configuración de los bytes de la imagen en función del endianness.
-    if (vars->img->endian == 1)
-    {
-        vars->img->addr[pixel + 0] = (color >> 24);
-        vars->img->addr[pixel + 1] = (color >> 16) & 0xFF;
-        vars->img->addr[pixel + 2] = (color >> 8) & 0xFF;
-        vars->img->addr[pixel + 3] = (color) & 0xFF;
-    }
-    else if (vars->img->endian == 0)
-    {
-        vars->img->addr[pixel + 0] = (color) & 0xFF;
-        vars->img->addr[pixel + 1] = (color >> 8) & 0xFF;
-        vars->img->addr[pixel + 2] = (color >> 16) & 0xFF;
-        vars->img->addr[pixel + 3] = (color >> 24);
-    }
+	if (y >= WIN_H || x >= WIN_W || y < 0 || x < 0)
+		return ;
+	pixel = (y * vars->img->sz_l) + (x * (vars->img->bpp / 8));
+	if (vars->img->endian == 1)
+	{
+		vars->img->addr[pixel + 0] = (color >> 24);
+		vars->img->addr[pixel + 1] = (color >> 16) & 0xFF;
+		vars->img->addr[pixel + 2] = (color >> 8) & 0xFF;
+		vars->img->addr[pixel + 3] = (color) & 0xFF;
+	}
+	else if (vars->img->endian == 0)
+	{
+		vars->img->addr[pixel + 0] = (color) & 0xFF;
+		vars->img->addr[pixel + 1] = (color >> 8) & 0xFF;
+		vars->img->addr[pixel + 2] = (color >> 16) & 0xFF;
+		vars->img->addr[pixel + 3] = (color >> 24);
+	}
 }
